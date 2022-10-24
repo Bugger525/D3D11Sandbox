5df674dd3124f7e91ac2d37d5bce4f0d8ceec7a0
Bugger <101984965+Bugger525@users.noreply.github.com>
2022-10-24 09:41:01 +0900

    Update Model.h

--- D3D11Sandbox/Engine/Model.h
+++ D3D11Sandbox/Engine/Model.h
@@ -15,15 +15,11 @@ public:
     Model(ID3D11Device* device);
     ~Model();
     
-	void Cleanup();
-	void Render(ID3D11DeviceContext* context);
+    void Cleanup();
+    void Render(ID3D11DeviceContext* context);
 
-	int GetIndexCount() const;
-private:
-    bool InitializeBuffers(ID3D11Device* device);
-    void CleanupBuffers();
-    void RenderBuffers(ID3D11DeviceContext* context);
-	
+    int GetIndexCount() const;
+private:	
     ID3D11Buffer* mVertexBuffer = nullptr;
     ID3D11Buffer* mIndexBuffer = nullptr;
     int mVertexCount= 0, mIndexCount = 0;
