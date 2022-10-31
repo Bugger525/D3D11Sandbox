4561e8b5884a92c83798e0fbd4c04d16375f1d5c
Bugger <101984965+Bugger525@users.noreply.github.com>
2022-10-31 09:44:14 +0900

    Update IndirectX.h

--- D3D11Sandbox/Engine/IndirectX.h
+++ D3D11Sandbox/Engine/IndirectX.h
@@ -1,17 +1,12 @@
 #pragma once
 
 #include <string>
+#include <wrl.h>
 #include <DirectXMath.h>
 
 struct HWND__;
-struct IDXGISwapChain;
 struct ID3D11Device;
 struct ID3D11DeviceContext;
-struct ID3D11RenderTargetView;
-struct ID3D11Texture2D;
-struct ID3D11DepthStencilState;
-struct ID3D11DepthStencilView;
-struct ID3D11RasterizerState;
 
 class IndirectX
 {
@@ -20,11 +15,11 @@ public:
 	~IndirectX();
 	void Cleanup();
 
-	void BeginScene(float r, float g, float b, float);
+	void BeginScene(float r, float g, float b, float a);
 	void EndScene();
 
-	ID3D11Device* GetDevice() const;
-	ID3D11DeviceContext* GetDeviceContext() const;
+	WRL::ComPtr<ID3D11Device> GetDevice() const;
+	WRL::ComPtr<ID3D11DeviceContext> GetDeviceContext() const;
 
 	const DirectX::XMMATRIX& GetProjectionMatrix() const;
 	const DirectX::XMMATRIX& GetWorldMatrix() const;
@@ -33,18 +28,25 @@ public:
 	std::string GetVideoCardName() const;
 	int GetVideoCardMemory() const;
 private:
+	struct IDXGISwapChai
+	struct ID3D11RenderTargetView;
+	struct ID3D11Texture2D;
+	struct ID3D11DepthStencilState;
+	struct ID3D11DepthStencilView;
+	struct ID3D11RasterizerState;
+	
 	bool mVsyncEnabled = false;
 	int mVideoCardMemory = 0;
 
 	std::string mVideoCardDescription;
-	IDXGISwapChain* mSwapChain = nullptr;
-	ID3D11Device* mDevice = nullptr;
-	ID3D11DeviceContext* mDeviceContext = nullptr;
-	ID3D11RenderTargetView* mRenderTargetView = nullptr;
-	ID3D11Texture2D* mDepthStencilBuffer = nullptr;
-	ID3D11DepthStencilState* mDepthStencilState = nullptr;
-	ID3D11DepthStencilView* mDepthStencilView = nullptr;
-	ID3D11RasterizerState* mRasterState = nullptr;
+	Microsoft::WRL::ComPtr<IDXGISwapChain> mSwapChain = nullptr;
+	Microsoft::WRL::ComPtr<ID3D11Device> mDevice = nullptr;
+	Microsoft::WRL::ComPtr<ID3D11DeviceContext> mDeviceContext = nullptr;
+	Microsoft::WRL::ComPtr<ID3D11RenderTargetView> mRenderTargetView = nullptr;
+	Microsoft::WRL::ComPtr<ID3D11Texture2D> mDepthStencilBuffer = nullptr;
+	Microsoft::WRL::ComPtr<ID3D11DepthStencilState> mDepthStencilState = nullptr;
+	Microsoft::WRL::ComPtr<ID3D11DepthStencilView> mDepthStencilView = nullptr;
+	Microsoft::WRL::ComPtr<ID3D11RasterizerState> mRasterState = nullptr;
 
 	DirectX::XMMATRIX mProjectionMatrix = DirectX::XMMATRIX();
 	DirectX::XMMATRIX mWorldMatrix = DirectX::XMMATRIX();
